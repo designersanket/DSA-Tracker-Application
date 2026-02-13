f5a59caac500cc51a0195ae0c7dc27fc8341f0e8
designersanket <sanketjagadale04@gmail.com>
2026-02-13 22:29:50 +0530

    server file change

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const jwt = require('jsonwebtoken');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://dsa-sanket.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
